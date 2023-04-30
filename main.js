19ad5a24fcf07e2e2aeaaf2b65781582138b288e
Caroline <wanguic49@gmail.com>
2023-05-01 00:09:51 +0300

    few changes made

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 let progress = document.getElementById("progress");
 let song = document.getElementById("song");
 let ctrlIcon = document.getElementById("ctrlIcon");
+
+song.onloadedmetadata = function() {
+    progress.max = song.duration;
+    progress.value = song.currentTime;
+
+}
